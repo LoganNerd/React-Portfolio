3198a3c5e038c51ea2ee4a7b7e0459accdb151fa
Your Name <you@example.com>
2023-03-24 11:50:15 -0600

    Updates

---
+++ precache-manifest.842fedd9b60983c6afb50e7771e5bb8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f1ccc8582b017099306",
+    "url": "/React-Portfolio/static/css/main.37ca313b.chunk.css"
+  },
+  {
+    "revision": "8f1ccc8582b017099306",
+    "url": "/React-Portfolio/static/js/main.8f1ccc85.chunk.js"
+  },
+  {
+    "revision": "2365806ba9c353f5a3a7",
+    "url": "/React-Portfolio/static/js/1.2365806b.chunk.js"
+  },
+  {
+    "revision": "ca3e7d0e520939e3fa2c",
+    "url": "/React-Portfolio/static/css/2.90a2efff.chunk.css"
+  },
+  {
+    "revision": "ca3e7d0e520939e3fa2c",
+    "url": "/React-Portfolio/static/js/2.ca3e7d0e.chunk.js"
+  },
+  {
+    "revision": "2cc14daa3aecbf934e3f",
+    "url": "/React-Portfolio/static/js/runtime~main.2cc14daa.js"
+  },
+  {
+    "revision": "bb0307f8e1f01e9873c26aeec31458b3",
+    "url": "/React-Portfolio/index.html"
+  }
+];
