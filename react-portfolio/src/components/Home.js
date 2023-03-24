0734efb4bb6b35874957de8332eb4a742f5be4da
Your Name <you@example.com>
2023-03-24 11:36:50 -0600

    adding finishing touches

--- react-portfolio/src/components/Home.js
+++ react-portfolio/src/components/Home.js
@@ -1,65 +1,60 @@
 import React from "react";
 import "./Home.css";
 import { DiJavascript1 } from "react-icons/di";
-import { FaHtml5 } from "react-icons/fa";
-import { BsFiletypeCss } from "react-icons/bs";
+import { FaHtml5, FaReact, FaNodeJs } from "react-icons/fa";
+import { BsFiletypeCss, BsFiletypeSql } from "react-icons/bs";
+import { SiApollographql, SiJquery } from "react-icons/si";
+import { AiFillApi } from "react-icons/ai";
 
 function Home() {
   return (
     <div class="grid-container">
       <div class="welcome">
-        <h1>Welcome to my Portfolio!</h1>
+        <h1>Logan Juul</h1>
       </div>
-      <div class="introContainer">
-        <div class="Introduction">
-          <h2>
-            I am a junior developer who thrives on collaboration and am always
-            eager to expand my knowledge in the field.
-          </h2>
-        </div>
-        <div class="icon-container">
-          <DiJavascript1 class="icon-java" />
-        </div>
+      <div class="smallIntro">
+        <h3>
+          Hi, welcome to my portfolio! I am a junior full stack web developer
+          who loves making beautiful websites! I have experience in quite a few
+          different languages and packages and love solving problems.
+        </h3>
       </div>
-      <div class="introContainer">
-        <div class="Introduction2">
-          <h2>
-            As a technology enthusiast, I have a profound passion for developing
-            sophisticated websites with an aesthetically pleasing interface.
-          </h2>
-        </div>
-        <div class="icon-container">
-          <FaHtml5 class="icon-html" />
-        </div>
+
+      <div class="icon-container">
+        <BsFiletypeCss class="icon-css" />
       </div>
-      <div class="introContainer">
-        <div class="Introduction3">
-          <h2>
-            I have gained a ton of knowledge through participation in complex
-            projects and solving issues which includes React, SQL and more!
-          </h2>
-        </div>
-        <div class="icon-container">
-          <BsFiletypeCss class="icon-css" />
-        </div>
+      <div class="icon-container">
+        <FaNodeJs class="icon-node" />
       </div>
-      <div class="introContainer">
-        <div class="Introduction4">
-          <h2>Learn more About Me!</h2>
-        </div>
-        <div class="icon-container"></div>
+      <div class="icon-container">
+        <DiJavascript1 class="icon-java" />
       </div>
-      <div class="introContainer">
-        <div class="Introduction5">
-          <h2>Check out my Portfolio</h2>
-        </div>
-        <div class="icon-container"></div>
+      <div class="icon-container">
+        <FaReact class="icon-react" />
       </div>
-      <div class="introContainer">
-        <div class="Introduction6">
-          <h2>Check out my Resume!</h2>
-        </div>
-        <div class="icon-container"></div>
+      <div class="icon-container">
+        <FaHtml5 class="icon-html" />
+      </div>
+      <div class="icon-container">
+        <BsFiletypeSql class="icon-sql" />
+      </div>
+      <div class="icon-container">
+        <SiApollographql class="icon-apollo" />
+      </div>
+      <div class="icon-container">
+        <SiJquery class="icon-query" />
+      </div>
+      <div class="icon-container">
+        <AiFillApi class="icon-api" />
+      </div>
+      <div class="learn">
+        <button className="infoButton">Learn more About me!</button>
+      </div>
+      <div class="port">
+        <button className="infoButton">Check out my Portfolio!</button>
+      </div>
+      <div class="contact">
+        <button className="infoButton">Contact me!</button>
       </div>
     </div>
   );
--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -11,22 +11,38 @@ function Portfolio() {
       </div>
 
       <Carousel infiniteLoop autoPlay>
-        <div>
-          <a href="https://github.com/johndean3326/Baby_journal"> </a>
-          <img src="/images/babyJournal.jpg" alt="Baby Journal" />
-          <h1 className="babyJournal">Baby Journal</h1>
+        <div class="slide">
+          <a href="https://baby-journal.herokuapp.com/login">
+            <img src="/images/babyJournal.jpg" alt="Baby Journal" />
+
+            <div class="project-description">
+              <p>Baby Journal</p>
+              <p>
+                A journal for keeping track of your baby's milestones and
+                memories.
+              </p>
+            </div>
+          </a>
         </div>
 
-        <div>
-          <a href="https://alester77.github.io/Project-Camp/"> </a>
-          <img src="/images/projectCamp.png" alt="Project Camp" />
-          <p className="projectCamp">Project Camp</p>
+        <div class="slide">
+          <a href="https://alester77.github.io/Project-Camp/">
+            <img src="/images/projectCamp.png" alt="Project Camp" />
+            <div class="project-description">
+              <p>Project Camp</p>
+              <p>A website for finding and booking campsites.</p>
+            </div>
+          </a>
         </div>
 
-        <div>
-          <a href="https://www.youtube.com/watch?v=KtrF90Ge3aI"> </a>
-          <img src="/images/youtubeVideo.png" alt="Shipwreck Dive" />
-          <p className="youtubeVideo">Shipwreck Dive</p>
+        <div class="slide youtubeVideo">
+          <a href="https://www.youtube.com/watch?v=KtrF90Ge3aI">
+            <img src="/images/youtubeVideo.png" alt="Shipwreck Dive" />
+            <div class="project-description">
+              <p>Shipwreck Dive</p>
+              <p>A video of a dive to a shipwreck off the coast of Florida.</p>
+            </div>
+          </a>
         </div>
       </Carousel>
     </div>
